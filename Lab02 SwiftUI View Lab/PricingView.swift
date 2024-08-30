7c34818c21ded5ef419ece729d690bff3013259f
Daniel Baroi <danielbaroi@cedarville.edu>
2024-08-30 15:03:13 -0400

    Initial Commit

---
+++ Lab02 SwiftUI View Lab/PricingView.swift
@@ -0,0 +1,20 @@
+//
+//  PricingView.swift
+//  Lab02 SwiftUI View Lab
+//
+//  Created by Daniel Baroi on 8/30/24.
+//
+
+import SwiftUI
+
+struct PricingView: View {
+    var body: some View {
+        RoundedRectangle(cornerRadius: 20)
+            .fill(Color.purple)
+            .padding()
+    }
+}
+
+#Preview {
+    PricingView()
+}
