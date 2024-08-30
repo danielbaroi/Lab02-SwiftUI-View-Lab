e676faf7bc3cb6b34e08ce098191431c873dd420
Daniel Baroi <danielbaroi@cedarville.edu>
2024-08-30 14:13:15 -0400

    Initial Commit

---
+++ Lab02 SwiftUI View Lab/Lab02_SwiftUI_View_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lab02_SwiftUI_View_LabApp.swift
+//  Lab02 SwiftUI View Lab
+//
+//  Created by Daniel Baroi on 8/30/24.
+//
+
+import SwiftUI
+
+@main
+struct Lab02_SwiftUI_View_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Lab02 SwiftUI View LabUITests/Lab02_SwiftUI_View_LabUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Lab02_SwiftUI_View_LabUITestsLaunchTests.swift
+//  Lab02 SwiftUI View LabUITests
+//
+//  Created by Daniel Baroi on 8/30/24.
+//
+
+import XCTest
+
+final class Lab02_SwiftUI_View_LabUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
